371f0e8702ffcfc754935242af64898aea072ca2
Elias Burgstaller <elias.burgstaller@gmx.at>
2018-04-17 19:33:01 +0200

    Added p5.js

---
+++ scripts/dotify.js
@@ -0,0 +1,14 @@
+alert("Hello World!");
+
+const DotifyStage = (function() {
+    // DataStructure
+
+    return {
+        update: function() {
+
+        },
+        draw: function() {
+
+        }
+    }
+})();
